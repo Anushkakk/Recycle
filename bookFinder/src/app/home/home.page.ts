2803b42b674516c24270a4e91f8df3e956c39157
Sean Barton <bartons3@tcnj.edu>
2019-10-25 21:42:16 +0100

    Working Normalize

--- bookFinder/src/app/home/home.page.ts
+++ bookFinder/src/app/home/home.page.ts
@@ -4,6 +4,9 @@ import { MapDisplayPage } from '../map-display/map-display.page';
 import { Routes } from '@angular/router';
 import { Router } from '@angular/router';
 
+// :: splitAt = number => Array<any>|string => Array<Array<any>|string>
+const splitAt = index => x => [x.slice(0, index), x.slice(index)]
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -38,17 +41,124 @@ export class HomePage {
   }
 
   normalize() {
+    var cutterNum = 0;
+
+    //Place the string in a local variable so it does not update the text box
     this.CALL_NUM_ONE = this.callNumber_one;
+
+    //Remove all spaces from the string
     this.CALL_NUM_ONE = this.CALL_NUM_ONE.replace(/ /g, "");
 
-    //Parse through string until first number, insert space before first number
-    //Continue parsing until . or end, if . place space before .
+    //Read the second character of the string (the first will always be a letter)
+
+      //If the second character is a number, place a space before the second character
+      if(this.CALL_NUM_ONE.charCodeAt(1) >= 48 && this.CALL_NUM_ONE.charCodeAt(1) <= 57) {
+        console.log("Second character is a number\n");
+        this.CALL_NUM_ONE = splitAt(1)(this.CALL_NUM_ONE).join(' ');
+      }
+
+      //Otherwise, place a space after the second character
+      else if((this.CALL_NUM_ONE.charCodeAt(1) >= 65 && this.CALL_NUM_ONE.charCodeAt(1) <= 90) ||
+      (this.CALL_NUM_ONE.charCodeAt(1) >= 97 && this.CALL_NUM_ONE.charCodeAt(1) <= 122)) {
+        console.log("Second character is a letter\n");
+        this.CALL_NUM_ONE = splitAt(2)(this.CALL_NUM_ONE).join(' ');
+      }
+
+    //Continue reading until a period (.) is found
+    for(var i = 2; i < this.CALL_NUM_ONE.length; ++i) {
+      if(i > 50)
+        break;
+      if(this.CALL_NUM_ONE.charAt(i) == '.' && cutterNum == 0) {
+        //Read the next character
+        if(this.CALL_NUM_ONE.charCodeAt(i + 1) >= 48 && this.CALL_NUM_ONE.charCodeAt(i + 1) <= 57) {            
+          //If it is a number, it's still part of the classification number
+          console.log(". is part of classification #\n");
+        }
+        else if((this.CALL_NUM_ONE.charCodeAt(i + 1) >= 65 && this.CALL_NUM_ONE.charCodeAt(i + 1) <= 90) ||
+        (this.CALL_NUM_ONE.charCodeAt(1) >= 97 && this.CALL_NUM_ONE.charCodeAt(1) <= 122)) {
+          //If it is a letter, it's the beginning of the cutter and a space is added before the period
+          console.log(". is beginning of cutter #\n");
+          this.CALL_NUM_ONE = splitAt(i)(this.CALL_NUM_ONE).join(' ');
+          console.log("CUTTER NUMBER: " + cutterNum);
+          cutterNum = 1;
+          console.log("CUTTER NUMBER: " + cutterNum);
+          i += 3;
+        }
+      }
+      //Continue reading in characters
+      //If a c or a C is found, check the next letter
+      if(this.CALL_NUM_ONE.charAt(i) == 'c' || this.CALL_NUM_ONE.charAt(i) == 'C') {
+        console.log("C found");
+        //If it's a period, its a copy number and a space is added before the c
+        if(this.CALL_NUM_ONE.charAt(i + 1) == '.') {
+          console.log("Copy number found");
+          this.CALL_NUM_ONE = splitAt(i)(this.CALL_NUM_ONE).join(' ');
+          i += 3;
+        }
+        //Otherwise, this is a second cutter and a space is added before the c
+        else {
+          console.log("Second cutter found");
+          this.CALL_NUM_ONE = splitAt(i)(this.CALL_NUM_ONE).join(' ');
+          i++;
+        }
+      }
+
+      //If a v or a V is found, check the next letter
+      if(this.CALL_NUM_ONE.charAt(i) == 'v' || this.CALL_NUM_ONE.charAt(i) == 'V') {
+        console.log("V found");
+        //If it's a period, its a version number and a space is added before the v
+        if(this.CALL_NUM_ONE.charAt(i + 1) == '.') {
+          console.log("Version number found");
+          console.log(this.CALL_NUM_ONE + '\t' + i);
+          this.CALL_NUM_ONE = splitAt(i)(this.CALL_NUM_ONE).join(' ');
+          i += 3;
+          console.log(this.CALL_NUM_ONE + '\t' + i);
+        }
+        //Otherwise, this is a second cutter and a space is added before the v
+        else {
+          console.log("Second cutter found");
+          this.CALL_NUM_ONE = splitAt(i)(this.CALL_NUM_ONE).join(' ');
+          i++;
+        }
+      }
+
+      //If a period is found, this is a second cutter, the period should be removed and a space
+      //is added before the letter
+
+      console.log(this.CALL_NUM_ONE + '\t' + i);
+
+      console.log("CUTTER NUMBER: " + cutterNum);
+      if(this.CALL_NUM_ONE.charAt(i) == '.' && cutterNum == 1) {
+        console.log("THIS IS THE ONE RIGHT HERE AHHHHHSecond cutter found");
+        var str1, str2;
+        str1 = this.CALL_NUM_ONE.substr(0, i);
+        str2 = this.CALL_NUM_ONE.substr(i);
+        str2 = str2.substr(1);
+        console.log("THIS IS STR2: " + str2);
+        this.CALL_NUM_ONE = str1 + ' ' + str2;
+        i += 2;
+        console.log(this.CALL_NUM_ONE + '\t' + i);
+      }
+      //If any other letter is found, (not c or v), this is a second cutter and a space is
+      //added before the letter
+      else if((this.CALL_NUM_ONE.charCodeAt(i + 1) >= 65 && this.CALL_NUM_ONE.charCodeAt(i + 1) <= 90) ||
+      (this.CALL_NUM_ONE.charCodeAt(1) >= 97 && this.CALL_NUM_ONE.charCodeAt(1) <= 122)) {
+        console.log("Second cutter found");
+        this.CALL_NUM_ONE = splitAt(i + 1)(this.CALL_NUM_ONE).join(' ');
+        i++;
+      }
+    }
+
+    console.log(this.CALL_NUM_ONE);
+
+      //Split the string up into an array of strings using a space (" ") as a delimeter
+
   }
 
   load() {
     this.normalize();
     var obj = {callnum: this.CALL_NUM_ONE, collection: this.collection_one};
-    
+    /*
     this.http.post("http://bookfind.hpc.tcnj.edu/retrieve-data.php", JSON.stringify(obj))
     .subscribe (data => {
       this.bookValueString = data['_body'];
@@ -63,6 +173,6 @@ export class HomePage {
       alert(error); 
     });
 
-    
+    */
   }
 }
