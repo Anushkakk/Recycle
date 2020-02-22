2d55fa157e65e23c5f3e31e59e710603581ceb97
sterly <deracys1@tcnj.edu>
2020-02-22 00:34:24 -0500

    Restored app to former glory by changing path of modal module

--- bookFinder/src/app/app-routing.module.ts
+++ bookFinder/src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ModalPageModule} from './modal/modal.module'
-import { MapDisplayPageModule } from './map-display/map-display.module';
-import { HomePageModule } from './home/home.module';
+import { ModalPageModule } from './modal/modal.module'
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'map-display/:data', loadChildren: './map-display/map-display.module#MapDisplayPageModule' },
-  { path: 'modal-page', loadChildren: './modal/modal-page.module#ModalPagePageModule' },
- // { path: 'modal', loadChildren: './modal/modal.module#ModalPageModule' },
+  { path: 'modal-page', loadChildren: './modal/modal.module' },
+  { path: 'modal', loadChildren: './modal/modal.module' },
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ModalPageModule, HomePageModule, MapDisplayPageModule],
+  imports: [RouterModule.forRoot(routes), ModalPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
